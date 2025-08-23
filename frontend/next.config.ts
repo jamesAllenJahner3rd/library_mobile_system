9e1e31da5ee29ecf5a59b8a3330e9a76b5294602
James Jahner <jitone1@gmail.com>
2025-08-22 21:46:06 -0500

    next config configed.

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,6 +1,7 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from "next/types";
 import path from "path";
 const nextConfig: NextConfig = {
+  reactStrictMode: true,
   turbopack: {
     root: path.join(__dirname, ".."),
   },
