5c66947ba0950fb6a2c1ebe0f58874983ab71d35
James Jahner <jitone1@gmail.com>
2025-08-22 22:04:09 -0500

    Made some comments

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,10 +1,10 @@
-import type { NextConfig } from "next/types";
-import path from "path";
+import type { NextConfig } from "next/types"; //had to add '/types'
+import path from "path"; //needs to Point to the right package.json because the project has a multiple package.json's
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   turbopack: {
-    root: path.join(__dirname, ".."),
+    root: path.join(__dirname, ".."), // path needed for this line
   },
 };
 
-export default nextConfig;
+export default nextConfig; // On this version a named object was needed: nextConfig: NextConfig = {
