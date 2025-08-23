29c32f692d90a41ecf2ce1a41ed86588e62a10bb
James Jahner <jitone1@gmail.com>
2025-08-22 21:18:02 -0500

    Capacitor is added and working.

---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.lms.www',
+  appName: 'lms',
+  webDir: 'build'
+};
+
+export default config;
