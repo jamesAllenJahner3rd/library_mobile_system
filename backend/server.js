358cc9db71f13cdef287d1034c60618ae1ba6586
James Jahner <jitone1@gmail.com>
2025-08-22 22:49:09 -0500

    added tsx to stop common JS error on: npm run Dev

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
 
 dotenv.config();
 
@@ -8,11 +8,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (_req, res) => {
-  res.send('Mobile LMS API is running');
+app.get("/", (_req, res) => {
+  res.send("Mobile LMS API is running");
 });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,19 @@
 import express from "express";
 import mongoose from "mongoose";
-import dotenv  from "dotenv";
+import dotenv from "dotenv";
 import usersRoutes from "./routes/users.routes";
+// import cors from "cors";
+dotenv.config();
+
+const app = express();
+// app.use(cors());
+app.use(express.json());
+
+app.get("/", (_req, res) => {
+  res.send("Mobile LMS API is running");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
