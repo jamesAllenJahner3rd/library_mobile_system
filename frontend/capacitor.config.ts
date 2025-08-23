316a9e72f03d5a1255a52b7d42b7858a9f15faa3
James Jahner <jitone1@gmail.com>
2025-08-22 20:43:58 -0500

    React/next installed and working.

--- frontend/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.james.mobilelms',
-  appName: 'mobile_lms',
-  webDir: 'dist'
-};
-
-export default config;
